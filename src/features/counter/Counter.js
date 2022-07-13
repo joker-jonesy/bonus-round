c124cf0275a74e41e8d4e87ecbe999eaf8fdec7d
joker-jonesy <spashmcgrath94@yahoo.com>
2022-07-13 15:34:53 -0400

    FontAwesome

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import {useSelector, useDispatch} from "react-redux";
 import {decrement, increment} from "./CounterSlice";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+
 
 export function Counter (){
     const count = useSelector((state)=> state.counter.value);
     const dispatch = useDispatch();
-    const [age, setAge]= React.useState(count)
 
     return(
         <div>
             <div>
-                <button onClick={()=>dispatch(increment())}>Increment</button>
-                <button onClick={()=>dispatch(decrement())}>Decrement</button>
+
+                <FontAwesomeIcon icon={"plus"} onClick={()=>dispatch(increment())} />
+                <FontAwesomeIcon icon={"minus"} onClick={()=>dispatch(decrement())}/>
+
+                {/*<button onClick={()=>dispatch(increment())}>Increment</button>*/}
+                {/*<button onClick={()=>dispatch(decrement())}>Decrement</button>*/}
             </div>
-            <div>
+            <div style={{color:"red", fontSize:"24px"}}>
                 <h1>{count}</h1>
             </div>
         </div>
